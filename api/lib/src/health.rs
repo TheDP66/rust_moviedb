6c564a081954e89d1b20794803be3a35320df8d0
Dharma Putra <dharma66putra@gmail.com>
2024-01-09 22:18:51 +0700

    test(_): add units and integration tests example

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -9,17 +9,25 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting version");
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
+// #[get("/version")]
+// async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+//     tracing::info!("Getting version");
+//     let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+//         .fetch_one(db.get_ref())
+//         .await;
 
-    match result {
-        Ok(version) => version,
-        Err(e) => format!("Error: {:?}", e),
-    }
+//     match result {
+//         Ok(version) => version,
+//         Err(e) => format!("Error: {:?}", e),
+//     }
+// }
+
+async fn version() -> HttpResponse {
+    tracing::info!("Getting version");
+    HttpResponse::Ok()
+        .append_header(("x-version", API_VERSION))
+        .append_header(("x-thankyou", "everyone live"))
+        .body(API_VERSION)
 }
 
 // ? Method 1
@@ -34,7 +42,7 @@ async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
 // ? Method 2: using macros
 async fn health() -> HttpResponse {
     HttpResponse::Ok()
-        .append_header(("x-version", "gamma"))
+        .append_header(("x-version", API_VERSION))
         .append_header(("x-thankyou", "everyone live"))
         .finish()
 }
@@ -44,5 +52,28 @@ pub fn service(cfg: &mut ServiceConfig) {
     // cfg.service(hello_world).service(version).service(health);
 
     // ? Method 2: using macros
-    cfg.route("/health", web::get().to(health));
+    cfg.route("/health", web::get().to(health))
+        .route("/version", web::get().to(version));
+}
+
+pub const API_VERSION: &str = "v0.0.1";
+
+#[cfg(test)]
+mod tests {
+    use actix_web::http::StatusCode;
+
+    use super::*;
+
+    // ? Unit test example
+    #[actix_rt::test]
+    async fn health_check_works() {
+        let res = version().await;
+
+        assert!(res.status().is_success());
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let data = res.headers().get("x-version").and_then(|h| h.to_str().ok());
+
+        assert_eq!(data, Some(API_VERSION));
+    }
 }
--- api/lib/src/lib.rs
+++ api/lib/src/lib.rs
@@ -1,1 +1,3 @@
+pub mod films;
 pub mod health;
+pub mod tests;
